acae4f22fb77d8b5b8296e4ab20d28f2f74c7951
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-08 17:08:09 -0600

    added in str for rooms when ref'd

--- src/room.py
+++ src/room.py
@@ -10,6 +10,9 @@ class Room:
         self.s_to = None
         self.e_to = None
         self.w_to = None
+    
+    def __str__(self):
+        return(F'You find yourself in the{self.name}. {self.flavor}')
 
 
 

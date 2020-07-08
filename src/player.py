7fbefa7899ce4d3fd6ea8f1f1f493913a895de3e
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-08 14:30:14 -0600

    first pass at room and player classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, currentRoom = 'outside'):
+        self.currentRoom = currentRoom
+
+testPlayer = Player()
+
+print(testPlayer.currentRoom)

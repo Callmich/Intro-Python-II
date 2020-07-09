d64749b4eb9e63eec1de09b3a1955a90ffdd488a
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-08 18:13:01 -0600

    added an attempt at a moving method for the player that will also lean on the attributes of a room

--- src/player.py
+++ src/player.py
@@ -5,6 +5,12 @@ class Player:
     def __init__(self, currentRoom, isPlaying = True):
         self.currentRoom = currentRoom
         self.isPlaying = isPlaying
-        
+    def move(self, compass):
+        travelTo = getattr(self.currentRoom, F'{compass}_to')
+        if travelTo == None:
+            print("You Can't Go That Way")
+        else:
+            self.currentRoom = travelTo
+            print(self.currentRoom)
 
 
